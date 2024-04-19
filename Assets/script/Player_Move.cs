f03b8d6ac95ab08981c68a08f6b4f4e385a716fb
Nguyễn Anh Quân <anhquan2712003@gmail.com>
2024-04-19 11:38:54 +0700

    4/19/2024_move

--- Assets/script/Player_Move.cs
+++ Assets/script/Player_Move.cs
@@ -1,8 +1,9 @@
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 10f;
     private Vector3 startTouchPosition, endTouchPosition;
 
     void Update()
@@ -17,8 +18,18 @@ public class Player_Move : MonoBehaviour
             endTouchPosition.z = 0;
 
             Vector3 direction = endTouchPosition - startTouchPosition;
+            Vector3 right = new Vector3(1, 0, 0);
+            Debug.Log(AngleBetweenTwoVectors(direction, right) );
+            if (AngleBetweenTwoVectors(direction, right) > 35 && AngleBetweenTwoVectors(direction, right) < 135){
+                direction = new Vector3(0, direction.y, 0);
+            } else if (AngleBetweenTwoVectors(direction,right)>135) {
+                direction = new Vector3(direction.x, 0, 0);
+            } 
+            else {
+                direction = new Vector3(direction.x, 0, 0);
+            }
             direction.Normalize();
-
+            Debug.Log(direction);
             GetComponent<Rigidbody2D>().velocity = direction * speed;
         }
     }
@@ -28,5 +39,16 @@ public class Player_Move : MonoBehaviour
         if (collision.gameObject.tag == "Wall") {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
+    }
+
+    public float AngleBetweenTwoVectors(Vector3 vec1, Vector3 vec2) {
+        float angle = Vector3.Dot(vec1, vec2);
+        float cosAngle = angle / (AbsVector(vec1) * AbsVector(vec2));
+        float radian = Mathf.Acos(cosAngle);
+        return radian * 180 / Mathf.PI;
+    }
+
+    public float AbsVector(Vector3 vec) {
+        return Mathf.Sqrt(vec.x * vec.x + vec.y * vec.y);
     }
 }
